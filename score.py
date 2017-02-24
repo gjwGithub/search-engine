5e52e71cddf59fcaf05fd8de2e389311d65de4ee
gjwGithub <gjwGithub@users.noreply.github.com>
2017-02-23 20:34:45 -0800

    Add files via upload

--- score.py
+++ score.py
@@ -10,7 +10,7 @@ import math
 from pprint import pprint
 import sys
 
-K = 10
+K = 100
 client = MongoClient()
 db = client.SearchEngine
 
@@ -20,6 +20,7 @@ bookkeeping = json.loads(f)
 #pprint(bookkeeping)
 
 def getScore(query):
+    start_time = time.time()
     words = tokenize.computeWordFrequencies(tokenize.tokenize(query))
     score = {}
     magnitude = {}
@@ -40,8 +41,6 @@ def getScore(query):
                     score[post['document']] += tfidf * post['tf-idf']
                 else:
                     score[post['document']] = tfidf * post['tf-idf']
-
-    start_time = time.time()
     
     VectorLength = {}
     #for post in db.VectorLength.find({}, {'_id': False}):
@@ -57,10 +56,37 @@ def getScore(query):
     results = []
     for i, document in enumerate(sorted_key_list):
         if i > K: break
-        print "Rank " + str(i) + ": " + document + ". Score: " + str(score[document])
-        results.append(bookkeeping[document[13:]])
-    return results
+        #print "Rank " + str(i) + ": " + document + ". Score: " + str(score[document])
+        # results.append({"url": bookkeeping[document[13:]], "title": getTitle(document), "abstract": 'Murray Sherk Murray Sherk Univ. of Waterloo, School of Computer Science msherk@dragon.uwaterloo.ca Author, editor, or reviewer of: Self-adjusting $k$-ary search-trees [ D. Eppstein publications ] [ Citation database ] [ Authors ] Fano Experimental Web Server, D. Eppstein , School of Information & Co...'})
+        results.append(getDocumentItem(document))
+    return results, time.time() - start_time
 
+def getDocumentItem(document):
+    post = db.DocumentItem.find({"document": document}, {'_id': False})
+    url = bookkeeping[document[13:]]
+    if post.count() == 0:
+        title = bookkeeping[document[13:]].split('/')[-1]
+        abstract = "Not available"
+    else:
+        title = post[0]['title']
+        if title == "":
+            title = bookkeeping[document[13:]].split('/')[-1]
+        abstract = post[0]['abstract']
+    return {"url": url, "title": title, "abstract": abstract}
+
+
+def getTitle(document):
+    post = db.TitleForwardIndex.find({"document": document}, {'_id': False})
+    if post.count() == 0:
+        return bookkeeping[document[13:]].split('/')[-1]
+    else:
+        title = ""
+        for p in post[0]['tokens']:
+            p = p.title()
+            title += p + " "
+        if title == "":
+            return bookkeeping[document[13:]].split('/')[-1]
+        return title
 
 def main(argv):
     if len(argv) >= 1:
--- server.py
+++ server.py
@@ -10,46 +10,26 @@ import random
 from flask import Flask, render_template, redirect
 from flask import request
 from flask import make_response
+import score
 
 
 app = Flask(__name__)
-print ('hahaha')
 
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['GET'])
 def search():
-    query = request.form['query']
-    lists = ["www.ics.uci.edu", "www.ics.uci.edu/faculty.html"]
-    return render_template('search.html', urls = lists)
+    query = request.args.get('query')
+    results, time = score.getScore(query)
+    length = len(results)
+    return render_template('search.html', results = results, time=time, length = length)
 
 @app.route('/', methods=['GET'])
 def index():
     author = "Me"
     name = "You"
     return render_template('index.html', author=author, name=name)
-    # req = request.args.lists()
-
-    # print("Request:" + str(req))
-    # #print(json.dumps(req, indent=4))
-
-    # res = {"content": "shabi"}
-
-    # res = json.dumps(res, indent=4)
-    # print(res)
-    # r = make_response(res)
-    # r.headers['Content-Type'] = 'application/json'
-    # return r
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-
-    # logging.basicConfig(level=logging.DEBUG,
-    #             format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-    #             datefmt='%a, %d %b %Y %H:%M:%S',
-    #             filename='app.log',
-    #             filemode='w')
-    # logging.debug('This is debug message')
-    # logging.info('This is info message')
-    # logging.warning('This is warning message')
 
     print "Starting app on port %d" % port
 
